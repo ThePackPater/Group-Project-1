2221f45130836e625d4956a83e0fc9f2f81e3d26
ThePackPater <thepackpater@gmail.com>
2019-03-12 01:23:59 -0500

    added choose your killer slide show

--- group.js
+++ group.js
@@ -1,43 +1,70 @@
+//slideshow JS
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
 var config = {
-    apiKey: "AIzaSyClMUA_cxxS07psp6djaLnDvM8ISajX7-I",
-    authDomain: "group-project-1-92136.firebaseapp.com",
-    databaseURL: "https://group-project-1-92136.firebaseio.com",
-    projectId: "group-project-1-92136",
-    storageBucket: "group-project-1-92136.appspot.com",
-    messagingSenderId: "830035537071"
+  apiKey: "AIzaSyClMUA_cxxS07psp6djaLnDvM8ISajX7-I",
+  authDomain: "group-project-1-92136.firebaseapp.com",
+  databaseURL: "https://group-project-1-92136.firebaseio.com",
+  projectId: "group-project-1-92136",
+  storageBucket: "group-project-1-92136.appspot.com",
+  messagingSenderId: "830035537071"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$("#add-runner-btn").on("click", function (event) {
+  event.preventDefault();
+
+  var runnerName = $("#name-input").val().trim();
+
+  var newRunner = {
+    name: runnerName
   };
-  firebase.initializeApp(config);
 
-  var database = firebase.database();
+  database.ref().push(newRunner);
 
-  database.ref().on("value", function(snapshot) {
-    if (snapshot.exists()) {
-        $("#add-runner-btn").hide();git 
-        $("#start-run-btn").show();
-      }else{
-        $("#add-runner-btn").show();
-        $("#start-run-btn").hide();
-        $("#")
-      }
+  //alert("Runner successfully added");
 
-  }, function(errorObject) {
-    console.log("Errors handled: " + errorObject.code);
+  $("#runner-name-input").val("");
+
   });
+database.ref().on("value", function (snapshot) {
+  if (snapshot.exists()) {
+    $("#add-runner-btn").hide();
+    $("#start-run-btn").show();
+  } else {
+    $("#add-runner-btn").show();
+    $("#start-run-btn").hide();
+    $("#")
+  }
 
+}, function (errorObject) {
+  console.log("Errors handled: " + errorObject.code);
+});
 
-  $("#add-runner-btn").on("click", function(event){
-    event.preventDefault();
-  
-    var runnerName = $("#name-input").val().trim();    
-  
-    var newRunner = {
-      name: runnerName      
-    };
-  
-    database.ref().push(newRunner);  
-  
-    //git aalert("Runner successfully added");
-  
-    $("#runner-name-input").val("");
-  
-  });
